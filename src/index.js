02703fee951ba1c6774d30722f117c84cf81f57f
Vladislav Flinn <vladislavflinn@gmail.com>
2023-08-14 14:32:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const str = `${Math.abs(n)}`;
+
+    let newStr = "";
+
+    for (const char of str) {
+        newStr = char + newStr;
+    }
+
+    return Number(newStr);
+};
